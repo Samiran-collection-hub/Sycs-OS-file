8ead02182374f8d973b5d3cacf03bfafccb53e75
Samiran-collection-hub <samirdhanke53@gmail.com>
2026-07-08 16:18:14 +0530

    Create fibon.java
    the 4 a practicale of OS

---
+++ fibon.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+class fibon
+{
+    public static void main (String[] args) {
+        int n=20;
+        int[] memo = new int[n+1];
+        for (int i=1; i<=n; i++){
+            System.out.print(fibonacci(i,memo)+" ");
+        } 
+    }
+    public static int fibonacci(int n, int[] memo){
+        if(memo[n]!=0) return memo[n];
+        if(n==1 || n==2) return 1;
+        else {
+            memo[n]=fibonacci(n-1, memo)+fibonacci(n-2, memo); return memo[n];
+        }
+    }
+}
