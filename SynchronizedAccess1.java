35dcb283e360a019f18191df7c7c9700c8b38459
Samiran-collection-hub <samirdhanke53@gmail.com>
2026-07-18 19:22:01 +0530

    Create SynchronizedAccess1.java
    practical 5 part 2

---
+++ SynchronizedAccess1.java
@@ -0,0 +1,35 @@
+class Counter
+{
+  private int c=0;
+  public void inc()
+  {
+    synchronized(this){c++;}
+  }
+  public int get(){ return c;}
+}
+
+public class SynchronizedAccess1
+{
+  public static void main (String[] args) {
+    Counter cnt=new Counter();
+    Thread t1=new Thread(
+      ()->{
+        for(int i=0;i<1000;i++) cnt.inc();
+      }
+    );
+    Thread t2=new Thread(
+      ()->{
+        for(int i=0;i<1000;i++) cnt.inc();
+      }
+    );
+    t1.start();
+    t2.start();
+    try {
+      t1.join();
+      t2.join();
+    } catch(InterruptedException e) {
+      e.printStackTrace();
+    }
+    System.out.println("Counter: "+cnt.get());
+  }
+}
