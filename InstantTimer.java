e9f005278476214e0ddf662e4c3ef1f13fcf5426
Samiran-collection-hub <samirdhanke53@gmail.com>
2026-08-19 20:13:12 +0530

    Update InstantTimer.java

--- InstantTimer.java
+++ InstantTimer.java
@@ -3,7 +3,7 @@ import java.time.*;
 public class InstantTimer{
   public static void methodToBeTimed(){
     try {Thread.sleep(1500);} 
-    catch(Exception e) {Thread.currentThread().interrupt();}
+    catch(InterruptedException e) {Thread.currentThread().interrupt();}
   }
   public static void main (String[] args) {
     Instant start = Instant.now();
